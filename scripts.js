ed659ba3db5d55273d9e4a217cb07c1a00ef79b7
Zishan <kenzeesh@gmail.com>
2026-05-02 19:55:44 +0100

    added search filter with onkeyup

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
+let data=[];
+
 async function fetchData() {
     try {
         const response = await fetch('/Product.json');
-        const data = await response.json();
-        const productDiv= document.getElementById("products");
-        data.forEach(product => {
-            const productElement = document.createElement('div');
-            productElement.innerHTML = `
-                <h2>${product.name}</h2>
-                <p>${product.category}</p>
-                <p>${product.price}</p>
-                <p>${product.inStock ? 'In Stock' : 'Out of Stock'}</p> 
-            `;
-            productDiv.appendChild(productElement);
-        });
+        data = await response.json();
+        displayProduct(data);
+
     }
     catch (error) {
         console.error('Error fetching data:', error);
     }
 }
+function displayProduct(product) {
+    const ProductContainer= document.getElementById('products');
+    ProductContainer.innerHTML = '';
+    product.forEach(item => {
+        const productElement = document.createElement('div');
+        productElement.innerHTML = `
+            <h2>${item.name}</h2>
+            <p>Price: $${item.price}</p>
+            <p>${item.category}</p>
+            <p>${item.inStock ? 'In stock' : 'Out of stock'}</p>
+        `;
+        ProductContainer.appendChild(productElement);
+    });
+}
+
+function filterProducts() {
+const element = document.getElementById('searchBar');
+const filter = data.filter(
+    product=> product.name.toLowerCase().includes(element.value.toLowerCase())|| product.category.toLowerCase().includes(element.value.toLowerCase())
+);
+displayProduct(filter);
+}
+
 fetchData();
